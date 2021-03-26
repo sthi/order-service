4ec4213711fb600c98512e0cbda64cdb3de33c7c
suthi <st6gmail.com>
2021-03-26 12:28:52 -0400

    first commit

---
+++ src/main/java/com/javathi/order/controller/OrderController.java
@@ -0,0 +1,29 @@
+package com.javathi.order.controller;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javathi.order.dao.OderDao;
+import com.javathi.order.entity.Order;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+	
+	private OderDao orderDao;
+	
+	@GetMapping("/")
+	public List<Order> getOrder(){
+		return orderDao.getOrders()
+				.stream().sorted(Comparator.comparing(Order::getPrice))
+				.collect(Collectors.toList());
+	}
+	
+	  
+
+}
---
+++ src/main/java/com/javathi/order/entity/Order.java
@@ -0,0 +1,19 @@
+package com.javathi.order.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Order {
+	
+	private int id;
+	private String name;
+	private int quantity;
+	private long price;
+
+}
